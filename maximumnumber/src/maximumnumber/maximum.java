ac88182a910b6308d1b787bcfebb228581011f80
hardip1602 <144582238+hardip1602@users.noreply.github.com>
2023-09-27 13:28:03 +0530

    Add files via upload

---
+++ maximumnumber/src/maximumnumber/maximum.java
@@ -0,0 +1,18 @@
+package maximumnumber;
+
+public class maximum {
+	
+	public static void main(String args[]) {
+
+		int a=10;
+		int b=50;
+		if(a<b) {
+			System.out.println(b +"is greater");
+		}
+		else {
+			System.out.println(a+" is greater");
+		}
+	}
+
+}
+
